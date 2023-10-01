69a428e620e53716505fc2fc7dd893c4b02e24c8
Amit Yadav <amityadavdtu@gmail.com>
2023-10-02 02:47:52 +0530

    Created a Library Backend Project and Added configuration to disable http enpoints

---
+++ src/main/java/com/project/springbootlibrary/Dao/BookDao.java
@@ -0,0 +1,12 @@
+package com.project.springbootlibrary.Dao;
+
+import com.project.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookDao extends JpaRepository<Book , Long> {
+
+
+
+}
---
+++ src/main/java/com/project/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package com.project.springbootlibrary.config;
+
+import com.project.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config , CorsRegistry corsRegistry){
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST , HttpMethod.PUT ,HttpMethod.DELETE,HttpMethod.PATCH};
+        config.exposeIdsFor(Book.class);
+        disableHttpMethos(Book.class , config , theUnsupportedActions);
+        /*Configure CORS Mapping*/
+        corsRegistry.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
+    }
+    private void disableHttpMethos(Class theClass , RepositoryRestConfiguration configuration , HttpMethod[] unsupportedActions){
+        configuration.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+    }
+}
