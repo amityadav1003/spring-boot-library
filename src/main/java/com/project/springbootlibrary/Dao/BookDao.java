840bbb636ec9030686b24bd2cd74b112f8e2425d
Amit Yadav <amityadavdtu@gmail.com>
2023-10-03 00:16:46 +0530

    Search Filter Implemented Using JPA

--- src/main/java/com/project/springbootlibrary/Dao/BookDao.java
+++ src/main/java/com/project/springbootlibrary/Dao/BookDao.java
@@ -1,12 +1,17 @@
 package com.project.springbootlibrary.Dao;
 
 import com.project.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface BookDao extends JpaRepository<Book , Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title") String title , Pageable pageable);
 
+    Page<Book> findByCategoryContaining(@RequestParam("category")String category , Pageable pageable);
 
 
 }
